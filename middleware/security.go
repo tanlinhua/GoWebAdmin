6eb74b0adbe6b241e826db5924434b85f665fdc2
tanlinhua <493383500@qq.com>
2021-05-03 17:27:53 +0800

    add tips

--- middleware/security.go
+++ middleware/security.go
@@ -8,6 +8,28 @@ import (
 func Safe() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// trace.Debug("middleware.Safe.start")
-		// tests/tests/html.go
 	}
 }
+
+// func TestSafe() bool {
+// 	test := html.EscapeString(`<script>alert("test")</script> select * from user`)
+// 	fmt.Println(test)
+// 	test2 := html.UnescapeString(test)
+// 	fmt.Println(test2)
+
+// 	if ok := FilteredSQLInject(test2); ok {
+// 		fmt.Println("存在sql注入风险")
+// 		return true
+// 	}
+// 	return false
+// }
+
+// // 正则过滤sql注入的方法，参数 : 要匹配的语句
+// func FilteredSQLInject(to_match_str string) bool {
+// 	str := `(?:')|(?:--)|(/\\*(?:.|[\\n\\r])*?\\*/)|(\b(select|update|and|or|delete|insert|trancate|char|chr|into|substr|ascii|declare|exec|count|master|into|drop|execute)\b)`
+// 	re, err := regexp.Compile(str)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	return re.MatchString(to_match_str)
+// }
