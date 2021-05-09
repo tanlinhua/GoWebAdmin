e8cc56a401d980dd07851ef53e7edd81b83a3d88
tanlinhua <493383500@qq.com>
2021-05-09 10:16:01 +0800

    整理cron

--- server/cron/clean.go
+++ server/cron/clean.go
@@ -1,1 +1,22 @@
 package cron
+
+import (
+	"fmt"
+	"time"
+)
+
+var specClean = "0 0 1 * * ?"    // 每天凌晨1点执行一次
+var specTest1 = "*/30 * * * * ?" // 每分钟的第30秒执行一次
+var specTest2 = "*/60 * * * * ?" // 每分钟的第60秒执行一次
+
+func cleanLog() {
+	fmt.Println("每天凌晨1点执行一次", time.Now())
+}
+
+func test1() {
+	fmt.Println("每分钟的第30秒执行一次!", time.Now())
+}
+
+func test2() {
+	fmt.Println("每分钟的第60秒执行一次!", time.Now())
+}
--- server/cron/cron.go
+++ server/cron/cron.go
@@ -1,24 +1,16 @@
 package cron
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/robfig/cron"
 )
 
 func Work() {
 	c := cron.New()
 
-	spec1 := "*/30 * * * * ?" //cron表达式，每30秒一次
-	c.AddFunc(spec1, func() {
-		fmt.Println("30s", time.Now())
-	})
+	c.AddFunc(specTest1, test1)
+	c.AddFunc(specTest2, test2)
 
-	spec3 := "*/60 * * * * ?" //cron表达式，每60秒一次
-	c.AddFunc(spec3, func() {
-		fmt.Println("60s", time.Now())
-	})
+	c.AddFunc(specClean, cleanLog) //每天凌晨1点执行一次
 
 	c.Start()
 	select {} //阻塞主线程停止
