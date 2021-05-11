94102c5dedc9636852e9ef0b41600da0dbc89b97
tanlinhua <493383500@qq.com>
2021-05-11 12:25:23 +0800

    add http server limit

---
+++ middleware/limit.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//Limit function is gin middleware to limit current requests
+func Limit(max int) gin.HandlerFunc {
+	if max <= 0 {
+		log.Panic("max must be more than 0")
+	}
+	sema := make(chan struct{}, max)
+	return func(c *gin.Context) {
+		var called, fulled bool
+		defer func() {
+			if called == false && fulled == false {
+				<-sema
+			}
+			if r := recover(); r != nil { // We don't handle panic
+				panic(r)
+			}
+		}()
+
+		select {
+		case sema <- struct{}{}:
+			c.Next()
+			called = true
+			<-sema
+		default:
+			fulled = true
+			c.Status(http.StatusBadGateway)
+		}
+	}
+}
