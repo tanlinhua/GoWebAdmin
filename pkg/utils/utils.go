3ef57868b8978803bb28f5a68e6508913e445055
tanlinhua <493383500@qq.com>
2021-05-24 23:22:45 +0800

    add func

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -198,3 +198,19 @@ func ArrayIntersect(array1 []string, arrayOthers ...[]string) []string {
 	}
 	return result
 }
+
+// 将传入的时间戳转为时间
+func TimeStampToData(timeStamp int64) string {
+	t := time.Unix(timeStamp, 0)
+	return t.Format("2006-01-02 15:04:05")
+}
+
+// string时间解析为Unix时间戳
+func Strtotime(str string) (int64, error) {
+	layout := "2006-01-02 15:04:05"
+	t, err := time.Parse(layout, str)
+	if err != nil {
+		return 0, err
+	}
+	return t.Unix(), nil
+}
