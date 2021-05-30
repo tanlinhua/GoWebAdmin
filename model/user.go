fb4a850c80f953b0b63c6fa9e1d93be84c49a772
tanlinhua <493383500@qq.com>
2021-05-31 00:00:22 +0800

    UserModel.v1

--- model/user.go
+++ model/user.go
@@ -1,9 +1,16 @@
 package model
 
-//
+// 用户模型
 type User struct {
+	Id       int    `form:"id" json:"id" validate:"numeric" swaggerignore:"true"`                      // ID
+	UserName string `form:"username" json:"username" uri:"username" xml:"username" binding:"required"` // 用户名
+	Password string `form:"password" json:"password" uri:"password" xml:"password" binding:"required"` // 密码
 }
 
-// 增删改查
-func (u *User) Add() {
+// 用户登录
+func (u *User) Login() (user_id int, msg string) {
+	if u.UserName != "test" || u.Password != "888" {
+		return 0, "用户名或密码错误"
+	}
+	return 1, "登录成功"
 }
