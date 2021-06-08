0cecf1d36e4de47fb9e5aab90ef295a6ff6e5db8
tanlinhua <493383500@qq.com>
2021-06-08 12:35:35 +0800

    调整demo

--- pkg/cron/clean.go
+++ pkg/cron/clean.go
@@ -6,17 +6,17 @@ import (
 )
 
 var specClean = "0 0 1 * * ?"    // 每天凌晨1点执行一次
-var specTest1 = "*/30 * * * * ?" // 每分钟的第30秒执行一次
-var specTest2 = "*/60 * * * * ?" // 每分钟的第60秒执行一次
+var specTest1 = "*/20 * * * * ?" // 每N秒执行一次
+var specTest2 = "0 */2 * * * ?"  // 每N分钟执行一次
 
 func cleanLog() {
-	fmt.Println("每天凌晨1点执行一次", time.Now())
+	fmt.Println("每天凌晨1点执行一次", specClean, time.Now())
 }
 
 func test1() {
-	fmt.Println("每分钟的第30秒执行一次!", time.Now())
+	fmt.Println("每N秒执行一次!", specTest1, time.Now())
 }
 
 func test2() {
-	fmt.Println("每分钟的第60秒执行一次!", time.Now())
+	fmt.Println("每N分钟执行一次!", specTest2, time.Now())
 }
