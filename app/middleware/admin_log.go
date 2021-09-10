04b2111e8cb6d13e6303bc2c7cbf1ab0fc528ee3
tanlinhua <493383500@qq.com>
2021-09-10 11:45:01 +0800

    body改成json

--- app/middleware/admin_log.go
+++ app/middleware/admin_log.go
@@ -1,14 +1,13 @@
 package middleware
 
 import (
-	"bytes"
-	"io/ioutil"
-	"net/url"
+	"encoding/json"
 	"strings"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/tanlinhua/go-web-admin/app/model"
+	"github.com/tanlinhua/go-web-admin/pkg/trace"
 	"github.com/tanlinhua/go-web-admin/pkg/utils"
 )
 
@@ -28,14 +27,14 @@ func AdminLog() gin.HandlerFunc {
 		if isBlackUri(c.Request.RequestURI) {
 			return
 		}
-
-		body, _ := c.GetRawData()
-		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-
 		c.Next()
-
 		// 只记录POST
 		if c.Request.Method == "POST" {
+			byteData, err := json.Marshal(c.Request.Form)
+			if err != nil {
+				trace.Error("middleware.AdminLog.json.Marshal.err=" + err.Error())
+				return
+			}
 			var adminLog model.AdminLog
 			session := sessions.Default(c)
 			admin_id := session.Get("adminId")
@@ -47,7 +46,7 @@ func AdminLog() gin.HandlerFunc {
 			}
 			adminLog.Uri = c.Request.RequestURI
 			adminLog.Title = getAdminLogTitle(c.Request.RequestURI)
-			adminLog.Body, _ = url.QueryUnescape(string(body))
+			adminLog.Body = string(byteData)
 			adminLog.Ip = c.ClientIP()
 			adminLog.Add()
 		}
